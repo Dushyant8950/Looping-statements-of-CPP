e023679b7fc3afb4bd570c06f3c495e10464fb9a
Dushyant Sharma <149956711+Dushyant8950@users.noreply.github.com>
2023-11-06 22:50:08 +0530

    while Looping statement

---
+++ ReverseCounting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+     cout<<"enter i:";
+    cin>>i;
+    int n=1;
+    while (n<=i){ 
+        cout<<i<<endl;
+         i--;
+}
+}
